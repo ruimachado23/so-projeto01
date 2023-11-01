3618c51b42d6d0b505fc0394b4e89cc16247b22c
ruimachado23 <rui.faria.machado@gmail.com>
2023-11-01 16:19:19 +0000

    size partido

--- spacecheck.sh
+++ spacecheck.sh
@@ -12,20 +12,21 @@ date=""
 limit=""
 dir=""
 options=""
+min_size="0"  # Initialize the minimum size to 0 by default
 
 # Processamento e Validação das opções da linha de comando
-while getopts "n:d:s:ral:" opt; do          # ":" após letra indica que necessita de argumento
-    case $opt in                            # variável $OPTARG usada para armazenar o argumento
+while getopts "n:d:s:ral:" opt; do
+    case $opt in
         n)
-            regex="$OPTARG"                                 # armazenar valores associados às respetivas variáveis 
-            options="$options -$opt \"$OPTARG\""            # guardar a opçao na string options
-            ;;                              
+            regex="$OPTARG"
+            options="$options -$opt \"$OPTARG\""
+            ;;
         d)
             date="$OPTARG"
             options="$options -$opt \"$OPTARG\""
             ;;
         s)
-            regex="$OPTARG"
+            min_size="$OPTARG"  # Store the minimum size specified with -s
             options="$options -$opt \"$OPTARG\""
             ;;
         r)
@@ -39,7 +40,7 @@ while getopts "n:d:s:ral:" opt; do          # ":" após letra indica que necessi
             options="$options -$opt \"$OPTARG\""
             ;;
         \?)
-            echo "Opção inválida: -$OPTARG" >&2     # erro: opcçao inválida
+            echo "Opção inválida: -$OPTARG" >&2
             exit 1
             ;;
     esac
@@ -48,7 +49,7 @@ done
 shift $((OPTIND - 1))       # "deslocar" ou "remover" as opções de linha de comando já processadas
                             # de modo a que o diretório fique guardado na variável $1
 if [ $# -ne 1 ]; then
-    echo "Erro: É necessário especificar um e um só diretório." # verificar se o utilizar introduziu um diretório
+    echo "Erro: É necessário especificar um e um só diretório." # verificar se o utilizador introduziu um diretório
     exit 1
 fi
 
@@ -59,14 +60,14 @@ if [ -z "$date" ]; then
 fi                          # que é uma expressão regular que corresponde a qualquer sequência de caracteres
 
 if [ -z "$regex" ]; then
-    regex=".*"              
+    regex=".*"
 fi
 
-# manipulacao das flags -r e -a 
-if [[ "$options" != *"-r"* ]] && [[ "$options" != *"-a"* ]]; then      # quando ambas as flags nao foram inseridas;           
+# manipulacao das flags -r e -a
+if [[ "$options" != *"-r"* ]] && [[ "$options" != *"-a"* ]]; then      # quando ambas as flags nao foram inseridas;
     sort_order="-k1,1nr"                                               # classifica os dados por ordem decrescente com base na primeira coluna (-k1,1)
 elif [[ "$options" == *"-r"* ]] && [[ "$options" != *"-a"* ]]; then    # quando a flag -r foi inserida, mas a -a nao:
-    sort_order="-k1,1n"                                                # classifica os dados em ordem crescente com base na primeira coluna (-k1,1) 
+    sort_order="-k1,1n"                                                # classifica os dados em ordem crescente com base na primeira coluna (-k1,1)
 elif [[ "$options" != *"-r"* ]] && [[ "$options" == *"-a"* ]]; then    # quando a flag -r nao foi inseridas, mas a -a sim;
     sort_order="-k2,2"                                                 # classifica os dados em ordem crescente com base na segunda coluna (-k2,2)
 elif [[ "$options" == *"-r"* ]] && [[ "$options" == *"-a"* ]]; then    # quando ambas as flags foram inseridas:
@@ -77,35 +78,32 @@ fi
 echo "SIZE NAME $(date +%Y%m%d) $options $dir"
 
 find "$dir" -type d | \
-    while read -r folder; do                                            # loop para encontrar os diretórios
-        size=0                                                          # inicializar a variável size
-        while IFS= read -r -d $'\0' file; do                            # loop para ver arquivos do diretório
-            if [[ -f "$file" && $(basename "$file") =~ $regex ]]; then  # verificaçao de expressao regular (-n)
-                if [[ "$date" != ".*" ]]; then                          # verificar se foi introduzida uma data
-                    file_date=$(date -r "$file" +%Y%m%d)                # obter a data de modifiçao do ficheiro
+    while read -r folder; do
+        size=0
+        while IFS= read -r -d $'\0' file; do
+            if [[ -f "$file" && $(basename "$file") =~ $regex ]]; then
+                if [[ "$date" != ".*" ]]; then
+                    file_date=$(date -r "$file" +%Y%m%d)
                     if [[ "$file_date" -ge "$date" ]]; then
                         file_size=$(du -b "$file" | cut -f1)
-                        if [ -n "$file_size" ]; then
-                            size=$((size + file_size))                  # adicionar o tamanho do ficheiro ao tamanho total
-                        else
-                            size="NA"                                   # se nao for possivel obter o tamanho do ficheiro, size = "NA"
-                        fi                    fi
+                        if [ -n "$file_size" ] && [ "$file_size" -ge "$min_size" ]; then
+                            size=$((size + file_size))
+                        fi
+                    fi
                 else
                     file_size=$(du -b "$file" | cut -f1)
-                    if [ -n "$file_size" ]; then
-                        size=$((size + file_size))                      # adicionar o tamanho do ficheiro ao tamanho total
-                    else
-                        size="NA"                                       # se nao for possivel obter o tamanho do ficheiro, size = "NA"
-                    fi                
+                    if [ -n "$file_size" ] && [ "$file_size" -ge "$min_size" ]; then
+                        size=$((size + file_size))
+                    fi
                 fi
-            fi  
+            fi
         done < <(find "$folder" -type f -print0)
-        if [ "$size" -gt 0 ]; then
-            echo "$size $folder"                                        # print do size e do respetivo diretório
+        if [ "$size" -ge "$min_size" ]; then  # Check if size is greater than or equal to the minimum size
+            echo "$size $folder"
         fi
     done | \
     sort $sort_order | \
-    if [ -n "$limit" ]; then                                            # limitar a quantidade de saidas
+    if [ -n "$limit" ]; then
         head -n "$limit"
     else cat
     fi
