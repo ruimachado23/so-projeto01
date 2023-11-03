f982c4def11410412fb9fe432bf8968c281ef3a3
Roldão <jroldao04@ua.pt>
2023-11-03 09:07:13 +0000

    o NEW funfa finalmente :))

--- spacerate.sh
+++ spacerate.sh
@@ -44,6 +44,13 @@ while read -r size path; do
    data2["$path"]=$size
 done < <(tail -n +2 "$2")
 
+# Merge the data from data1 and data2
+for path in "${!data2[@]}"; do
+    if [ -z "${data1[$path]}" ]; then
+        data1["$path"]=""
+    fi
+done
+
 # Print the headers
 echo "SIZE NAME"
 
@@ -54,19 +61,19 @@ show_difference() {
     local size2=${data2[$path]}
 
     if [ -z "$size1" ] && [ -z "$size2" ]; then
-        echo "0 $path"                              # sem alteração no tamanho  - size1 e size2 vazios
+        echo "0 $path"
     elif [ -z "$size1" ]; then
-        echo "$size2 $path NEW"                     # diretório/arquivo adicionado - size1 está vazio
+        echo "$size2 $path NEW"
     elif [ -z "$size2" ]; then
-        echo "$size1 $path REMOVED"                 # diretório/arquivo removido - size2 está vazio
+        echo "$size1 $path REMOVED"
     else
         local diff=$((size2 - size1))
         if [ "$diff" -gt 0 ]; then
-            echo "$size2 $path NEW"             # diretório/arquivo adicionado - diff é positivo
+            echo "$diff $path"
         elif [ "$diff" -lt 0 ]; then
-            echo "$size1 $path REMOVED"         # diretório/arquivo removido - diff é negativo
+            echo "$diff $path"
         else
-            echo "0 $path"                      # sem alteração no tamanho
+            echo "0 $path"
         fi
     fi
 }
@@ -82,9 +89,9 @@ elif [ "$reverse_sort" = true ] && [ "$alphabetical_sort" = false ]; then
 elif [ "$reverse_sort" = false ] && [ "$alphabetical_sort" = true ]; then
     for path in "${!data1[@]}"; do
         show_difference "$path" 
-    done | sort -k2                                 # alfabeticamente by path
+    done | sort -k1,1nr                            # numeric sorting by size (descending order)
 elif [ "$reverse_sort" = false ] && [ "$alphabetical_sort" = false ]; then
     for path in "${!data1[@]}"; do
         show_difference "$path"
-    done | sort -k1,1nr                            # numeric sorting by size (descending order)
+    done | sort -k1,1nr -k2                      # Sort by size (descending) and then by path
 fi
